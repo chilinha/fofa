378c8e5a09ae8c91824f2145eb9c65cea718e6f9
chilinha <69139070+chilinha@users.noreply.github.com>
2024-12-04 15:01:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ ip_version_priority = "ipv4"
 
 source_urls = [
     "https://raw.githubusercontent.com/chilinha/fofa/refs/heads/main/speedtest/txt/Tianjin_liantong.txt",
-    "https://raw.githubusercontent.com/chilinha/fofa/refs/heads/main/speedtest/txt/Beijing_liantong.txt",
 ]
 
 url_blacklist = [
