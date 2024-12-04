89ba7f5b234e6f3fbb05021cc0e6c12541417d29
chilinha <69139070+chilinha@users.noreply.github.com>
2024-12-04 12:54:42 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+ip_version_priority = "ipv4"
+
+source_urls = [
+    "https://github.com/zjykfy/yangfeiyue/raw/main/speedtest/txt/Tianjin_liantong.txt",
+    "https://github.com/zjykfy/yangfeiyue/raw/main/speedtest/txt/Beijing_liantong.txt",
+]
+
+url_blacklist = [
+
+]
+
+announcements = [
+    {
+        "channel": "公告",
+        "entries": [
+            {"name": "更新日期", "url": "", "logo": ""},
+        ]
+    }
+]
+
+epg_urls = [
+    "https://epg.pw/xmltv/epg_TW.xml",
+]
